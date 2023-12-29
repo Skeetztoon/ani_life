68c91215051e71c66efaa30b1f6f330ec901b3ca
skeetztoon <1014day@gmail.com>
2023-12-29 19:10:47 +0300

    added categoryName field to category class

--- lib/components/chipsCarousel.dart
+++ lib/components/chipsCarousel.dart
@@ -3,63 +3,70 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class Category {
   final String id;
+  final String categoryName;
   bool selected;
 
-  Category(this.id, this.selected);
+  Category(this.id, this.categoryName, this.selected);
 }
 
 class CategoryNotifier extends StateNotifier<Category> {
-  CategoryNotifier(String id) : super(Category(id, false));
+  CategoryNotifier(Category category)
+      : super(Category(category.id, category.categoryName, false));
 
   void toggleSelected() {
     state = Category(
       state.id,
+      state.categoryName,
       !state.selected,
     );
   }
 }
 
-final itemProvider = StateNotifierProvider.family<CategoryNotifier, Category, String>((ref, itemId) => CategoryNotifier(itemId));
+final itemProvider =
+    StateNotifierProvider.family<CategoryNotifier, Category, Category>(
+        (ref, category) => CategoryNotifier(category));
 
+final List<Category> categoryList = [
+  Category("Магазины", "stores", false),
+  Category("Клиники", "vets", false),
+  Category("Передержка", "care", false),
+  Category("Приюты", "shelters", false),
+  Category("Площадки", "playground", false)
+];
 
-class ChipCarousel extends ConsumerWidget {
+class ChipCarousel extends StatelessWidget {
   ChipCarousel({super.key});
-  final List<String> categoryList = ["Магазины", "Клиники", "Передержка", "Приюты", "Площадки"];
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-          child:
-          ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: categoryList.length,
-              itemBuilder: (context, index) {
-                return Consumer(
-                    builder: (context, ref, child) {
-                      final item = ref.watch(itemProvider(categoryList[index]));
-                      return
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 4),
-                          child: FilterChip(
-                            label: Text(item.id),
-                            selected: item.selected,
-                            onSelected: (bool value) => ref.read(itemProvider(item.id).notifier).toggleSelected(),
-                            backgroundColor: Colors.white,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20.0),
-                              side: BorderSide(
-                                color: Color(0xFFF39191),
-                                width: 2.0,
-                              ),
-                            ),
-                            // avatar: const Text("A"),
-                          ),
-                        );
-                    }
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: categoryList.length,
+            itemBuilder: (context, index) {
+              return Consumer(builder: (context, ref, child) {
+                final item = ref.watch(itemProvider(categoryList[index]));
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 4),
+                  child: FilterChip(
+                    label: Text(item.id),
+                    selected: item.selected,
+                    onSelected: (bool value) => ref
+                        .read(itemProvider(categoryList[index]).notifier)
+                        .toggleSelected(),
+                    backgroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                      side: BorderSide(
+                        color: Color(0xFFF39191),
+                        width: 2.0,
+                      ),
+                    ),
+                    // avatar: const Text("A"),
+                  ),
                 );
-              }
-          )
-      );
+              });
+            }));
   }
 }
