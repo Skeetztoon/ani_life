d8f90ad7a3c1d6d78bcfd61811e9fd8847056f89
Skeetztoon <1014day@gmail.com>
2024-03-17 23:43:12 +0300

    Added post creating feature

--- lib/components/profile/posts/post_model.dart
+++ lib/components/profile/posts/post_model.dart
@@ -1,23 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart%20';
 import 'package:intl/intl.dart';
-
-class Post {
-  final String authorEmail; // через currenUser!.email
-  final String authorNick; // через ???
-  final String authorImage; // через ???
-  final String createDate; // через ???
-  final String postText; //  из textEdit
-  final String postImage; // как аву
-
-  Post(this.postImage,
-      {required this.authorEmail,
-      required this.authorNick,
-      required this.authorImage,
-      required this.createDate,
-      required this.postText});
-}
 
 final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 final currentUser = FirebaseAuth.instance.currentUser;
@@ -25,7 +8,6 @@ final currentUser = FirebaseAuth.instance.currentUser;
 Future<bool> createPost(String postText) async {
   var userDoc = await _firestore.collection("users").doc(currentUser!.email).get();
   Map<String, dynamic> userData = userDoc.data() as Map<String, dynamic>;
-  print("получены данные пользователя");
   Map<String, dynamic> postData = {
     "authorEmail": currentUser!.email,
     "authorNick": userData["username"],
@@ -34,12 +16,9 @@ Future<bool> createPost(String postText) async {
     "createDate": DateFormat.yMd().add_Hm().format(DateTime.now()),
   };
   try {
-    print("Попытка отправить данные");
     await _firestore.collection("posts").add(postData);
-    print("Данные отправлены");
     return true;
   } catch (e) {
-    print("Что-то пошло не так");
     print(e);
   }
   return false;
